6d64d5434e2b1ca375c8b9b49caf9e031c1cf5e5
wylfred <wylfredtapouamo@gmail.com>
2026-06-28 14:28:21 +0100

    modifier du fichier UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -1,5 +1,28 @@
 <?php
 
+// namespace Database\Seeders;
+
+// use App\Models\User;
+// use Illuminate\Database\Seeder;
+// use Illuminate\Support\Facades\Hash;
+
+// class UserSeeder extends Seeder
+// {
+//     public function run(): void
+//     {
+//         if (!User::where('email', 'wylfreddev@gmail.com')->exists()) {
+//             User::create([
+//                 'name'     => 'Wylfred dev',
+//                 'email'    => 'wylfreddev@gmail.com',
+//                 'password' => Hash::make('wylfreddev'),
+//                 'role'     => 'super_admin',
+//                 'is_active' => true,
+//             ]);
+//         }
+//     }
+// }
+
+
 namespace Database\Seeders;
 
 use App\Models\User;
@@ -10,14 +33,14 @@ class UserSeeder extends Seeder
 {
     public function run(): void
     {
-        if (!User::where('email', 'superadmin@cachet.com')->exists()) {
-            User::create([
-                'name'     => 'Wylfred dev',
-                'email'    => 'wylfreddev@gmail.com',
-                'password' => Hash::make('wylfreddev'),
-                'role'     => 'super_admin',
+        User::firstOrCreate(
+            ['email' => 'wylfreddev@gmail.com'], // critère de recherche
+            [
+                'name'      => 'Wylfred dev',
+                'password'  => Hash::make('wylfreddev'),
+                'role'      => 'super_admin',
                 'is_active' => true,
-            ]);
-        }
+            ]
+        );
     }
 }
