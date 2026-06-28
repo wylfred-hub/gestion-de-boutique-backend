1fde38ae2130bec2777f0f343d58098f1922c2fc
wylfred <wylfredtapouamo@gmail.com>
2026-06-28 14:05:03 +0100

    activage du seeder dans docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,7 +11,7 @@ echo "==> Running migrations..."
 php artisan migrate --force
 
 echo "==> Running seeders..."
-# php artisan db:seed --force
+php artisan db:seed --force
 
 echo "==> Starting server on port 10000..."
 exec php -S 0.0.0.0:10000 -t public
